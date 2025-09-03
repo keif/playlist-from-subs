d137131097da23ae422de200f46f02c090baccd3
Keith Baker <github@baker.is>
2025-09-03 10:14:36 -0400

    fix(bash): we were clearing logs before directory creation

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 source venv/bin/activate
 
-: > logs/playlist-sync.log
-
 # Ensure logs directory exists
 mkdir -p logs
+
+# Clear previous log
+: > logs/playlist-sync.log
+
 python main.py --limit 50 >> logs/playlist-sync.log 2>&1
