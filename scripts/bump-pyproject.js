b625e595eb1a9c74cf7c89ba6a323c06b6bf0017
Keith Baker <github@baker.is>
2026-06-27 22:38:00 -0400

    fix(release): anchor pyproject version regex to [project] table

--- scripts/bump-pyproject.js
+++ scripts/bump-pyproject.js
@@ -1,4 +1,4 @@
-const VERSION_RE = /(\[project\][\s\S]*?\n\s*version\s*=\s*")([^"]+)(")/;
+const VERSION_RE = /(\[project\](?:(?!\n\[)[\s\S])*?\n\s*version\s*=\s*")([^"]+)(")/;
 
 module.exports = {
   readVersion(contents) {
@@ -7,9 +7,8 @@ module.exports = {
     return m[2];
   },
   writeVersion(contents, version) {
-    if (!VERSION_RE.test(contents)) {
-      throw new Error("version not found under [project] in pyproject.toml");
-    }
+    const m = contents.match(VERSION_RE);
+    if (!m) throw new Error("version not found under [project] in pyproject.toml");
     return contents.replace(VERSION_RE, `$1${version}$3`);
   },
 };
